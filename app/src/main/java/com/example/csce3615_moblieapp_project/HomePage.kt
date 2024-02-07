a3ae85149591424bddb28e610dd1bc0200d81254
StarlingStarter <Myles.work@bennettes.com>
2024-02-07 11:32:17 -0600

    added the other classes that are needed, and changed the main class to homepage class

--- app/src/main/java/com/example/csce3615_moblieapp_project/HomePage.kt
+++ app/src/main/java/com/example/csce3615_moblieapp_project/HomePage.kt
@@ -1,6 +1,5 @@
 package com.example.csce3615_moblieapp_project
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,10 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.csce3615_moblieapp_project.ui.theme.CSCE3615_MoblieApp_ProjectTheme
@@ -48,16 +44,22 @@ class Main : ComponentActivity() {
     @Composable
     fun appHomeScreen() {
         CSCE3615_MoblieApp_ProjectTheme {
-            Image(painter = painterResource(id = R.drawable.home_screen), contentDescription = null, contentScale = ContentScale.Crop, alpha = 1f)
-            MainCampus()
+            Image(
+                painter = painterResource(id = R.drawable.home_screen),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                alpha = 1f
+            )
+            MainCampusMenuDisplay()
+            DiscoveryPark()
         }
     }
-
+}
     //Takes you to the Main Campus class
     @Composable
-    fun MainCampus(modifier: Modifier = Modifier
+    fun MainCampusMenuDisplay(modifier: Modifier = Modifier
         .fillMaxSize()
-        .wrapContentSize(Alignment.CenterStart)){
+        .wrapContentSize(Alignment.Center)){
         var isButtonClicked by remember {mutableStateOf(false)}
 
         Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
@@ -69,22 +71,23 @@ class Main : ComponentActivity() {
             }
             if(isButtonClicked == true)
             {
-                Image(painter = painterResource(R.drawable.test_img), contentDescription = "Tepig")
+                Image(painter = painterResource(R.drawable.test_img), contentDescription = "just a test button")
             }
         }
-    }
+        }
+
 
     //Takes you to the discovery park class
     @Composable
     fun DiscoveryPark(modifier: Modifier = Modifier
         .fillMaxSize()
-        .wrapContentSize(Alignment.CenterStart)){
+        .wrapContentSize(Alignment.BottomCenter)){
         var isButtonClicked by remember {mutableStateOf(false)}
 
         Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
         {
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
             Button(onClick = {isButtonClicked = !isButtonClicked}) {
                 //Text(stringResource(R.string.avesta))
             }
@@ -94,5 +97,3 @@ class Main : ComponentActivity() {
             }
         }
     }
-
-}
--- app/src/main/java/com/example/csce3615_moblieapp_project/MainCampus.kt
+++ app/src/main/java/com/example/csce3615_moblieapp_project/MainCampus.kt
@@ -1,2 +1,41 @@
 package com.example.csce3615_moblieapp_project
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.csce3615_moblieapp_project.ui.theme.CSCE3615_MoblieApp_ProjectTheme
+
+class MainCampus : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CSCE3615_MoblieApp_ProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    appHomeScreen()
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun appHomeScreen() {
+        CSCE3615_MoblieApp_ProjectTheme {
+            Image(painter = painterResource(id = R.drawable.main_campus), contentDescription = null, contentScale = ContentScale.Crop, alpha = 1f)
+        }
+    }
+
+}
